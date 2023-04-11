8ab4d03191afc5ff0e0d3d15aecf863e5ff3850c
MasonJ-A <azzoparm@mcmaster.ca>
2023-04-11 14:34:00 -0400

    first test case added

--- pathfinder/src/test/java/ca/mcmaster/pathfinder/AppTest.java
+++ pathfinder/src/test/java/ca/mcmaster/pathfinder/AppTest.java
@@ -4,27 +4,25 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+import ca.mcmaster.pathfinder.Graph.Edge;
+import ca.mcmaster.pathfinder.Graph.Graph;
+import ca.mcmaster.pathfinder.Graph.Node;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-
-    /**
-     * @return the suite of tests being tested
-     */
-
-    /**
-     * Rigourous Test :-)
-     */
     @Test
-    public void testApp()
+    public void testequals()
     {
-        assertTrue( true );
+        Graph<Node> G = new Graph<>();
+        Node n1 = new Node(1.0, 1.0);
+        Node n2 = new Node(2.0, 2.0);
+        G.addEdge(n1, n2);
+        G.addEdge(n1, n2);
+        assertTrue(n1.equals(n1));
+        assertTrue(n2.equals(n2));
+        assertTrue(G.getEdge(0).equals(G.getEdge(1)));
     }
 }
